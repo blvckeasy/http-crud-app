a7a2837685a08015ba8bc53ea75adbc410afd3f2
blvckeasy <abdurakhmonovislom9@gmail.com>
2022-10-26 18:40:30 +0500

    adapted to clean code rules v1.2

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,89 +1,100 @@
+const users = [
+  // default users
+  { username: "blvckeasy", password: "123456" },
+  { username: "hakim", password: "b1Ui" },
+  { username: "helloworld", password: "randomo1" },
+]
+
 export class UserController {
   
-  users = [
-    // default users
-    { username: "blvckeasy", password: "123456" },
-    { username: "hakim", password: "b1Ui" },
-    { username: "helloworld", password: "randomo1" },
-  ]
+
+  // custom functions
+  /**
+   * @param {*} req - request
+   * @param {*} res - response
+   * @whyUse this function is used to combine repeated codes for login and registration  
+  */
+  static #userValidator(req) {
+    const { username, password } = req.body
+    
+    if (!(username && password)) {
+      return { 
+        status: 400,
+        ok: false,
+        message: "username and password is require!"
+      }
+    }
+    
+    const found_user = users.find((user) => {
+      if (username == user.username && password == user.password) {
+        return user
+      }
+    })
+
+    return {
+      status: 200,
+      ok: true,
+      data: found_user
+    }
+  }
 
   // GET
   static getUsers (_, res) {
-    res.write(JSON.stringify(this.users, null, 4));
+    res.write(JSON.stringify(users, null, 4));
     return { is_response_sended: true }
   }
 
   // POST
   static login (req, res) {
-    const { username, password } = req.body
-    
-    if (!(username && password)) {
+    const validate = this.#userValidator(req)
+    const { data: user } = validate
+
+    if (!validate.ok) {
+      res.write(JSON.stringify(validate))
+    }
+
+    if (user) {
       res.write(JSON.stringify({
         status: 200,
-        ok: false,
-        message: "username and password is required!"
+        ok: true,
+        message: "user found",
+        data: user
       }, null, 4))
-      return { is_response_sended: true }
-    }
-    
-    const found_user = this.users.find((user) => {
-      if (user.username == username && user.password == password) {
-        return user
-      }
-    })
-
-    if (!found_user) {
+    } else {
       res.write(JSON.stringify({
         status: 400,
         ok: false,
         message: "user not found.",
         data: {}
       }, null, 4))
-      
-    } else {
-      res.write(JSON.stringify({
-        status: 200,
-        ok: true,
-        message: "user found",
-        data: found_user
-      }, null, 4))
     }
     return { is_response_sended: true }
   }
 
   static registration (req, res) {
-    const { username, password } = req.body
-  
-    if (!(username && password)) {
-      res.write(JSON.stringify({
-        status: 200,
-        ok: false,
-        message: "username and password is required!"
-      }, null, 4))
-      return { is_response_sended: true }
+    const validate = this.#userValidator(req);
+    const { data: user } = validate;
+    const { username, password } = req.body;
+
+    if (!validate.ok) {
+      res.write(JSON.stringify(validate))
     }
 
-    const found_user = this.users.find((user) => {
-      if (user.username == username && user.password == password) {
-        return user
-      }
-    })
-
-    if (found_user) {
+    if (!user) {
+      const new_user = { username, password };
+      users.push(new_user);
+      res.write(JSON.stringify({
+        status: 200,
+        ok: true,
+        data: new_user
+      }, null, 4))
+    } else {
       res.write(JSON.stringify({
         status: 400,
         ok: false,
         message: "user already signed!",
         data: {}
       }, null, 4));
-    } else {
-      const new_user = { username, password };
-      this.users.push(new_user);
-      res.write(JSON.stringify({
-        status: 200,
-        ok: true,
-        data: new_user
-      }, null, 4))
     }
     return { is_response_sended: true }
   }
